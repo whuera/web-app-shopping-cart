ff0eead0506d84a483c1e2d9811ea72a9bc60b9e
William Huera <whuera@gmail.com>
2022-04-18 16:42:00 -0500

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-    productoURL: 'http://localhost:8082/'
-   // productoURL: 'https://web-app-shopping-cart.azurewebsites.net/'
+    // productoURL: 'http://localhost:8082/'
+    productoURL: 'https://calm-rock-0d0c8e20f.1.azurestaticapps.net/'
 };
